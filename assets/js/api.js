b64ebc73f88ebe0ab4fbafdebe5918e003e2ffcd
julio-olivares-porfin <julio@por-fin.cl>
2024-05-13 05:24:42 -0400

    subiendo prueba

---
+++ assets/js/api.js
@@ -0,0 +1,15 @@
+const API_URL = 'https://mindicador.cl/api'
+
+const fetchData = async (endpoint) =>{
+  try{
+    const response = await fetch (`${API_URL}/${endpoint}`)
+    if(!response.ok){
+      throw new Error('Error al obtener los datos')
+    }
+    return await response.json()
+  }catch(error){
+    throw new Error('Error al realizar la solicitud a la API')
+  }
+}
+console.log(fetchData)
+export {fetchData}
